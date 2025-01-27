826fa486b73aa9f77512b882eb5ef68e85510534
Burak Haşıcı <burakhasici@gmail.com>
2025-01-27 18:24:31 +0300

    feat: Add React and Solid.js integrations with Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
+import react from '@astrojs/react';
+import solidJs from '@astrojs/solid-js';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	experimental: {
+		assets: true,
+	},
+	integrations: [
+		react({
+			include: ['**/react/*'],
+		}),
+		solidJs({
+			include: ['**/solid/*', '**/node_modules/@suid/material/**'],
+		}),
+	],
+});
---
+++ src/stores/cart.ts
@@ -0,0 +1,30 @@
+import { computed, map } from 'nanostores';
+
+export const cart = map<Record<number, CartItem>>({});
+
+export function addItemToCart(item: ShopItem) {
+	const cartItem = cart.get()[item.id];
+	const quantity = cartItem ? cartItem.quantity : 0;
+
+	cart.setKey(item.id, { item, quantity: quantity + 1 });
+}
+
+export function removeItemFromCart(item: ShopItem) {
+	// @ts-ignore
+	cart.setKey(item.id, undefined);
+}
+
+export const subtotal = computed(cart, (entries) => {
+	let subtotal = 0;
+	Object.values(entries).forEach((entry) => {
+		if (!entry) {
+			return;
+		}
+
+		subtotal += entry.item.price * entry.quantity;
+	});
+
+	return subtotal;
+});
+
+
